7f5b9a37f13cee9db63d86342640e40035998d7e
Hitesh Pagare <94035444+hiteshkwg@users.noreply.github.com>
2022-09-03 00:32:07 +0530

    Pattern 19 solved

--- Patterns/Pattern19.java
+++ Patterns/Pattern19.java
@@ -1,4 +1,17 @@
 package Patterns;
+/* 
+    **********
+    ****  ****
+    ***    ***
+    **      **
+    *        *
+    *        *
+    **      **
+    ***    ***
+    ****  ****
+    **********
+
+*/
 
 import java.util.Scanner;
 
@@ -7,21 +20,28 @@ public class Pattern19 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // first half of inside diamond
-        int spaces = 0;
-        for(int i=1;i<=n;i++){
-            int tempSpaces = spaces;
+        for(int i=0;i<n;i++){
             for(int j=1;j<=n*2;j++){   
-                
-                else{
+                if(j<=n-i || j>n+i){
                     System.out.print("*");
+                }    
+                else{
+                    System.out.print(" "); 
                 }  
             }
-            spaces += 2;
             System.out.println(); 
         }
         // second half of inside diamond
         for(int i=1;i<=n;i++){
-
+            for(int j=1;j<=n*2;j++){   
+                if(j<=i || j>=(n*2)-(i-1)){
+                    System.out.print("*");
+                }    
+                else{
+                    System.out.print(" "); 
+                }  
+            }
+            System.out.println(); 
         }
         sc.close();
     }
