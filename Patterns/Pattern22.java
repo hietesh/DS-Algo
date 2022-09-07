acfeaa9a8b43302f6e75120eab7bf684f8fbaabc
Hitesh Pagare <hitesh.pagare@uplers.in>
2022-09-08 00:14:36 +0530

    Pattern 22 Completed

--- Patterns/Pattern22.java
+++ Patterns/Pattern22.java
@@ -6,15 +6,32 @@ public class Pattern22 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=0;i<(n*2)-1;i++){
+        // first half of the pattern
+        for(int i=0;i<n;i++){
             int nums = n;
-            for(int j=0;j<(n*2)-1;j++){
+            for(int j=1;j<=n;j++){
                 System.out.print(nums+" ");
-                if(j<i || j<=i){nums--;}
+                if(j<=i){nums--;}
             }
-            nums--;
+            for(int j=1;j<n;j++){
+                if(j>=n-i){nums++;}
+                System.out.print(nums+" ");
+            }
             System.out.println();
-        }    
+        }
+        // second half of the pattern
+        for(int i=n-1;i>=0;i--){
+            int nums = n;
+            for(int j=1;j<=n;j++){
+                System.out.print(nums+" ");
+                if(j<=i){nums--;}
+            }
+            for(int j=1;j<n;j++){
+                if(j>=n-i){nums++;}
+                System.out.print(nums+" ");
+            }
+            System.out.println();
+        }
         sc.close();
     }
 }
